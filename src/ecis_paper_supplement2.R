95cb82b120bf31082db17f022fd4d1e5dde22295
Taavi Päll <tapa741@gmail.com>
2015-11-24 14:20:53 +0200

    seems good

--- src/ecis_paper_supplement2.R
+++ src/ecis_paper_supplement2.R
@@ -1,5 +1,5 @@
 
-## @knitr munge_ecis
+## @knitr load_sirna_ecis
 
 library(plyr);library(dplyr)
 library(RSQLite)
@@ -15,7 +15,7 @@ library(ggthemes)
 db <- src_sqlite("data/ECIS2.sqlite", create = FALSE)
 
 # Look for table names 
-src_tbls(db) # list tables in database
+# src_tbls(db) # list tables in database
 
 # Extract siRNA experiments
 dates <- tbl(db, "Metadata") %>% filter(value=="siCD44") %>% select(Date) %>% distinct() %>% collect() %>% as.data.frame() %>% .[,"Date"]
@@ -47,9 +47,121 @@ ecis.sirna$treatment[ecis.sirna$treatment=="SCR"] <- "siNTP"
 ecis.sirna$treatment <- factor(ecis.sirna$treatment, levels = c("siNTP", "siCD44", "siVIM", "UT"))
 ecis.sirna$Freq <- factor(ecis.sirna$Freq, levels = unique(ecis.sirna$Freq), labels = paste(unique(ecis.sirna$Freq), "Hz"))
 
-# Number of experiments per condition
-# ecis.sirna %>% select(Date, GF, conc_GF) %>% 
-#   group_by(GF, conc_GF) %>% summarise(N = length(unique(Date)))
+# Plot function ----
+myalpha <- 0.3
+Myplot <- function(x) {
+  ylabeller <- function(parameter) {
+    if(parameter=="C"){out<-bquote(list(Capacitance,F))}
+    if(parameter=="Z"){out<-bquote(list(Impedance,Omega))}
+    if(parameter=="R"){out<-bquote(list(Resistance,Omega))}
+    out}
+  ggplot(x, aes(timeBin, value, color = treatment)) + 
+    facet_grid(Freq ~ conc_GF, scales = "free") +
+    stat_summary(fun.data = mean_se, geom = "errorbar", alpha = myalpha) +
+    stat_summary(fun.y = mean, geom = "line", size = 1) +
+    annotate("rect", xmin = -16, xmax = 0, ymin = -Inf, ymax = Inf, alpha = myalpha) +
+    ylab(ylabeller(unique(x$Param))) + 
+    xlab(bquote(list(Time~relative~to~release,h)))}
+
+# Filter and summarise ----
+Freq.subset <- c("1000 Hz","4000 Hz","16000 Hz","64000 Hz")
+ecis.sirna.supp <- ecis.sirna %>% 
+  select(Date, GF, conc_GF) %>% 
+  group_by(GF, conc_GF) %>% summarise(N = length(unique(Date))) %>%
+  filter(N > 1) %>% select(GF, conc_GF) %>% # filter series with N > 1
+  inner_join(ecis.sirna, .) %>% # use only data with N > 1
+  group_by(Param, Freq, Date, conc_GF, GF, treatment, timeBin) %>%
+  summarise(value = mean(value, na.rm = T)) %>% ungroup %>%
+  filter(Freq %in% Freq.subset, Param == Parameter, timeBin) %>% {
+    finito <- ddply(.,"timeBin", summarise, N = unique(Date) %>% length) %>%
+      filter(N>1) %>% max
+    .[.$timeBin <= finito,]
+  }
+
+# Imepedance plots ----
+vegf <- ecis.sirna.supp %>% filter(GF %in% c("VEGF","FBS_5")) %>% Myplot + scale_color_colorblind()
+fgf2 <- ecis.sirna.supp %>% filter(GF %in% c("bFGF","FBS_5")) %>% Myplot + theme(axis.title.y = element_blank())
+
+g <- ggplotGrob(vegf + theme(legend.title = element_blank(), legend.position="bottom"))$grobs
+legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
+
+common.theme <- theme(legend.position="none", plot.title = element_text(size = 11))
+supp.imp <- arrangeGrob(arrangeGrob(
+  vegf + ggtitle("VEGF, ng/ml") + common.theme,
+  fgf2 + ggtitle("FGF2, ng/ml") + common.theme + scale_color_colorblind(),
+  ncol = 2),
+  legend,
+  nrow=2, heights = c(12,1))
+
+# grid.draw(supp.imp)
+
+# Rb modeling ----
+rbdata <- tbl(db, "Rbmodel")
+sirna.rbdata <- rbdata %>% filter(Date %in% dates)
+ecis.rbsirna <- sirna.rbdata %>% collect %>% left_join(., wide.metadata)
+colnames(ecis.rbsirna)[1] <- "Time"
+
+# reset time and create time bins
+ecis.rbsirna %<>% left_join(., sirna.marks)
+
+rbsirna <- ecis.rbsirna %>% 
+  group_by(Date) %>%
+  mutate(Time = Time-Mark) %>% 
+  mutate(timeBin = cut(Time, 
+                       breaks = unique(floor(Time)),
+                       labels = unique(floor(Time))[-1],
+                       include.lowest=TRUE)) %>%
+  ungroup() %>% filter(complete.cases(.)) 
+
+rbsirna %<>% mutate(conc_GF = as.numeric(as.character(conc_GF)),
+                    timeBin = as.numeric(as.character(timeBin)))
+rbsirna$treatment[rbsirna$treatment=="SCR"] <- "siNTP"
+rbsirna$treatment <- factor(rbsirna$treatment, levels = c("siNTP", "siCD44", "siVIM", "UT"))
+
+MyRbplot <- function(x){ 
+  ggplot(x, aes(timeBin, value)) +
+    # geom_point(size = 0.5) +
+    stat_summary(fun.y = mean, geom = "line") +
+    stat_summary(fun.data = mean_se, geom = "errorbar", alpha = myalpha) +
+    annotate("rect", xmin = -16, xmax = 0, ymin = -Inf, ymax = Inf, alpha = .2) +
+    facet_grid(treatment ~ conc_GF) +
+    xlab(bquote(list(Time~relative~to~release,h))) +
+    ylab(bquote(list(R[b],Omega%*%cm^2)))}
+
+rbsirna.filt <- rbsirna %>% select(Date, GF, conc_GF) %>% 
+  group_by(GF, conc_GF) %>% summarise(N = length(unique(Date))) %>%
+  filter(N > 1) %>% select(GF, conc_GF) %>% # filter series with N > 1
+  inner_join(rbsirna, .) %>% # use only data with N > 1
+  group_by(Date, Param, GF, conc_GF, treatment, timeBin) %>%
+  summarise(value = mean(value, na.rm = T)) %>% ungroup %>% {
+    finito <- ddply(.,"timeBin", summarise, N = unique(Date) %>% length) %>%
+      filter(N > 1) %>% max
+    .[.$timeBin<=finito,]} %>% filter(Param == "Rb (ohm.cm^2)")
+
+# Barrier resistance
+vegf.Rb <- rbsirna.filt %>% filter(GF %in% c("VEGF","FBS_5")) %>% MyRbplot
+fgf.Rb <- rbsirna.filt %>% filter(GF %in% c("bFGF","FBS_5")) %>% MyRbplot
+
+supp.Rb <- arrangeGrob(
+  vegf.Rb + ggtitle("VEGF, ng/ml") + theme(plot.title = element_text(size = 11)),
+  fgf.Rb + ggtitle("FGF2, ng/ml") + theme(axis.title.y = element_blank(), plot.title = element_text(size = 11)),
+  ncol = 2,
+  widths = c(1,1))
+
+# grid.draw(supp.Rb)
+
+# Letters
+
+labels <- lapply(LETTERS[1:10], 
+                 function(letter) textGrob(letter, x=unit(0,"npc"), hjust = 0))
+
+supp <- arrangeGrob(arrangeGrob(labels[[1]], supp.imp, heights = c(1,12)),
+                    arrangeGrob(labels[[2]], supp.Rb, heights = c(1,12)),
+                    nrow = 2,
+                    heights = c(5,4))
+
+# grid.draw(supp)
+png("graphs/SuppFig4.png", w = 600, h = 800); plot(supp); dev.off()
 
 # # Migration ----
 # allmarks <- tbl(db, "Marks") %>% filter(Date %in% dates) %>%
@@ -165,121 +277,3 @@ ecis.sirna$Freq <- factor(ecis.sirna$Freq, levels = unique(ecis.sirna$Freq), lab
 #                         ncol = 4,
 #                         widths = c(7,7,7,1))
 # grid.draw(micro.plot) 
-
-# Plot functions ----
-Myplot <- function(x) {
-  
-  ylabeller <- function(parameter) {
-    if(parameter=="C"){out<-bquote(list(C, Capacitance~group("(",F,")")))}
-    if(parameter=="Z"){out<-bquote(list(Z, Impedance~group("(",Omega,")")))}
-    if(parameter=="R"){out<-bquote(list(R, Resistance~group("(",Omega,")")))}
-    out
-  }
-  
-  ggplot(x,aes(timeBin, value, color=treatment)) + 
-    facet_grid(Freq ~ conc_GF, scales = "free") +
-    stat_summary(fun.y = mean, geom = "line") +
-    stat_summary(fun.data = mean_se,geom = "errorbar") +
-    annotate("rect", xmin = -16, xmax = 0, ymin = -Inf, ymax = Inf, alpha = .2) +
-    ylab(ylabeller(unique(x$Param))) + 
-    xlab(bquote(list(Time~relative~to~release,h)))
-}
-
-Freq.subset <- c("1000 Hz","4000 Hz","16000 Hz","64000 Hz")
-
-# Imepedance plots ----
-siCD44.VEGF.list <- ecis.sirna %>% 
-  filter(!(GF=="bFGF"&conc_GF==8),!(GF=="VEGF"&conc_GF==79)) %>%
-  filter(GF%in%c("VEGF","FBS_5"), Freq %in% Freq.subset) %>% 
-  group_by(Param) %>% do(plots=Myplot(.)) %>% .$plots
-
-vegf <- siCD44.VEGF.list[[3]] + scale_color_colorblind()
-
-siCD44.FGF2.list <- ecis.sirna %>% 
-  filter(!(GF=="bFGF"&conc_GF==8),!(GF=="VEGF"&conc_GF==79)) %>%
-  filter(GF%in%c("bFGF","FBS_5"), Freq %in% Freq.subset) %>% 
-  group_by(Param) %>% do(plots=Myplot(.)) %>% .$plots
-
-fgf2 <- siCD44.FGF2.list[[3]] + scale_color_colorblind() + theme(axis.title.y = element_blank())
-
-plots <- list(vegf,fgf2)
-g <- ggplotGrob(plots[[1]] + theme(legend.title = element_blank(), legend.position="bottom"))$grobs
-legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
-
-plots <- lapply(plots, function(x) x + theme(legend.position="none"))
-supp.imp <- arrangeGrob(arrangeGrob(
-  plots[[1]] + ggtitle("VEGF, ng/ml") + theme(plot.title = element_text(size = 11)),
-  plots[[2]] + ggtitle("FGF2, ng/ml") + theme(plot.title = element_text(size = 11)),
-  ncol = 2),
-  legend,
-  nrow=2, heights = c(12,1))
-
-# grid.draw(supp.imp)
-
-# Rb modeling ----
-
-rbdata <- tbl(db, "Rbmodel")
-sirna.rbdata <- rbdata %>% filter(Date %in% dates)
-ecis.rbsirna <- sirna.rbdata %>% collect %>% left_join(., wide.metadata)
-colnames(ecis.rbsirna)[1] <- "Time"
-
-# reset time and create time bins
-ecis.rbsirna %<>% left_join(., sirna.marks)
-
-rbsirna <- ecis.rbsirna %>% 
-  group_by(Date) %>%
-  mutate(Time = Time-Mark) %>% 
-  mutate(timeBin = cut(Time, 
-                       breaks = unique(floor(Time)),
-                       labels = unique(floor(Time))[-1],
-                       include.lowest=TRUE)) %>%
-  ungroup() %>% filter(complete.cases(.)) 
-
-rbsirna %<>% mutate(conc_GF = as.numeric(as.character(conc_GF)),
-                    timeBin = as.numeric(as.character(timeBin)))
-rbsirna$treatment[rbsirna$treatment=="SCR"] <- "siNTP"
-rbsirna$treatment <- factor(rbsirna$treatment, levels = c("siNTP", "siCD44", "siVIM", "UT"))
-
-MyRbplot <- function(x){ 
-  ggplot(x, aes(timeBin, value)) +
-  geom_point(size = 0.5) +
-  stat_summary(fun.y = mean, geom = "line") +
-  stat_summary(fun.data = mean_se, geom = "errorbar") +
-  annotate("rect", xmin = -16, xmax = 0, ymin = -Inf, ymax = Inf, alpha = .2) +
-  facet_grid(treatment ~ conc_GF) +
-  xlab(bquote(list(Time~relative~to~release,h))) +
-  ylab(bquote(list(R[b],Omega%*%cm^2)))}
-
-# Barrier resistance
-vegf.Rb <- rbsirna %>% 
-  filter(!(GF=="bFGF"&conc_GF==8),!(GF=="VEGF"&conc_GF==79)) %>%
-  filter(GF %in% c("VEGF","FBS_5"), Param == "Rb (ohm.cm^2)") %>% 
-  # group_by(Date, timeBin, concGF, treatment) %>% summarise(value = mean(value, na.rm = T)) %>%
-  MyRbplot
-
-fgf.Rb <- rbsirna %>% 
-  filter(!(GF=="bFGF"&conc_GF==8),!(GF=="VEGF"&conc_GF==79)) %>%
-  filter(GF %in% c("bFGF","FBS_5"), Param == "Rb (ohm.cm^2)") %>% 
-  # group_by(Date, timeBin, concGF, treatment) %>% summarise(value = mean(value, na.rm = T)) %>%
-  MyRbplot
-
-supp.Rb <- arrangeGrob(
-  vegf.Rb + ggtitle("VEGF, ng/ml") + theme(plot.title = element_text(size = 11)),
-  fgf.Rb + ggtitle("FGF2, ng/ml") + theme(axis.title.y = element_blank(), plot.title = element_text(size = 11)),
-  ncol = 2,
-  widths = c(1,1))
-
-# grid.draw(supp.Rb)
-
-# Letters
-
-labels <- lapply(LETTERS[1:10], 
-                 function(letter) textGrob(letter, x=unit(0,"npc"), hjust = 0, gp = gpar(fontface=2)))
-
-supp <- arrangeGrob(arrangeGrob(labels[[1]], supp.imp, heights = c(1,12)),
-                    arrangeGrob(labels[[2]], supp.Rb, heights = c(1,12)),
-                    nrow = 2,
-                    heights = c(5,4))
-
-# grid.draw(supp)
-png("graphs/SuppFig4.png", w = 600, h = 800); plot(supp); dev.off()
